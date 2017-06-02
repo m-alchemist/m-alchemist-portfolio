e3eca791345981fc476aa451b5644300a1bbccd5
m-alchemist <alkhamis@email.arizona.edu>
2017-06-02 11:05:23 -0700

    some additions

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 var express=require('express');
+var path=require('path');
 
 
 
 var app=module.exports=express();
 
 var port = process.env.PORT || 3000;
-
 app.use(express.static(__dirname +'/public'));
 app.use(express.static(__dirname +'/style'));
 app.use(express.static(__dirname +'/files'));
+
+app.get('/projects',function(req,res,next){
+  res.sendFile(path.join(__dirname, './public', 'projects.html'));
+})
 
 app.listen(port,function(){
   console.log('listening on port '+port);
